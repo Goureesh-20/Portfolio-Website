0ef18b731f002b890d693eb525edc2dc961cbc9a
Goureesh-20 <goureeshyarlagadda@gmail.com>
2023-08-03 23:38:44 -0700

    0803-2

--- views/script.js
+++ views/script.js
@@ -25,3 +25,20 @@ document.addEventListener("DOMContentLoaded", function () {
         seeMoreBtn.style.display = "none"; // Hide the "See more" button after revealing the work items
       });
     });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbykuYNpe5muhYML9Kt-i43JfCitDAYEf9oWL_9FHXVdVzHwhpWvnUkmOlj2OmybhD8V/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.innerHTML = "Message sent successfully"
+      setTimeout(function(){
+        msg.innerHTML = ""
+      },5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
